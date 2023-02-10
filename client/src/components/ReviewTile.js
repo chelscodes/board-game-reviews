a3cb71d711fa9f150f0caac1f3cf0876bb38f05c
Chelsea Smith <chelsea.beth11@gmail.com>
2023-02-10 14:27:03 -0500

    added stars for ratings

--- client/src/components/ReviewTile.js
+++ client/src/components/ReviewTile.js
@@ -4,21 +4,20 @@ const ReviewTile = props => {
   const { rating, comment } = props
   const userName = props.user.userName
 
-  // let starRating = ""
-  // for (let i = 1; i <= 5; i++) {
-  //   if (i <= rating) {
-  //     starRating += "full star _ "
-  //   } else {
-  //     starRating += <i className="fa-regular fa-star"></i>
-  //   }
-  // }
+  let starRating = ""
+  for (let i = 1; i <= 5; i++) {
+    if (i <= rating) {
+      starRating += "★"
+    } else {
+      starRating += "☆"
+    }
+  }
 
   return (
     <div className="callout grid-x grid-padding-x reviews_tile">
       <div className="cell small=12 medium-4 large-3 review_user-rating">
         <h5 className="text-center review_username">{userName}</h5>
-        <p className="text-center">{rating}/5 stars</p>
-        {/* <div><i className="fa-sharp fa-solid fa-star"></i></div> */}
+        <p className="text-center stars">{starRating}</p>
       </div>
       <p className="cell small=12 medium-8 large-9 review_comment">{comment}</p>
     </div>
